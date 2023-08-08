f38f9ca47a5658ee0a763347bdc779a158e06398
cashartle1 <139818745+cashartle1@users.noreply.github.com>
2023-08-08 12:38:02 -0500

    updated script.js deleted code
    removed practice code from exercise from the file

--- js/script.js
+++ js/script.js
@@ -1,19 +1,4 @@
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
 alert('Hello World!');
 
-let simpleAddition = 2+6;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2; 
-document.write(doubleSize);
-
-let size = 100;
-let doubleSize = size * 2; 
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
 let favoriteFood = 'Gnocchi & Sausage'
 document.write(favoriteFood);
