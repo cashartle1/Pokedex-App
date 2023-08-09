52a0e48385c29d049f12d19e6f0dd005ca945a64
cashartle1 <139818745+cashartle1@users.noreply.github.com>
2023-08-09 15:41:06 -0500

    updated script.js to add pokemonList
    added array list with 3 types of Pokémon.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 alert('Hello World!');
 //this array will contain Pokemon data to be displayed
+let pokemonList = [
+    {name: 'Bulbasaur', height: 2, type: ['grass' , 'poison']},
+    {name: 'Zapdos', height: 5, type: ['electric' , 'flying']},
+    {name: 'Articuno', height: 5, type: ['ice' , 'flying']}
+];
+
 
 let favoriteFood = 'Gnocchi & Sausage'
 document.write(favoriteFood);
