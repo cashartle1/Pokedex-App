e57765d3339fa88e5ba359c403f74698ab7263c3
cashartle1 <139818745+cashartle1@users.noreply.github.com>
2023-08-10 16:45:31 -0500

    added loop for pokemonList
    loop added to style.js page for exercise1.3 to display pokemon names & heights on page

--- js/script.js
+++ js/script.js
@@ -6,4 +6,12 @@ let pokemonList = [
     {name: 'Dragonite', height: 7, type: ['dragon', 'flying']}
 ];
 
+// loop to display pokemon list
+for (let i = 0; i < pokemonList.length; i++) 
+    if(pokemonList[i].height < 5) {
+        document.write ( '<p>' + pokemonList[i].name + ' - height (ft): ' + pokemonList[i].height)
+    }else if (pokemonList[i].height >= 5) {  
+        // conditional to highlight big pokemon
+        document.write ('<p>' + pokemonList[i].name + ' - height (ft): ' + pokemonList[i].height + ' ...Wow, that\'s a big Pokemon!'); 
+    }
 
